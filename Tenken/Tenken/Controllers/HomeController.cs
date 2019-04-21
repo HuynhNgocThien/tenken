0e54f7ae68e486c87e87ebe2d35eb51aff0920db
HuynhNgocThien <huynhngocthien1995@gmail.com>
2019-04-21 22:43:07 +0700

    [Thien] Add Project

---
+++ Tenken/Tenken/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Tenken.Models;
+
+namespace Tenken.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = new Category() {
+                CategoryID = 1,
+                CategoryName = "Your application description page."
+            };
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Tenken/Tenken/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Tenken.MvcApplication" Language="C#" %>
